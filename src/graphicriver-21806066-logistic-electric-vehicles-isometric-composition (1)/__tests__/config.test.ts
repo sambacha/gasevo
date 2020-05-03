fc0737bc3f8efdac1849d0e42c0aa73a38cb5360
Freight Trust <4074885-freighttrust@users.noreply.gitlab.com>
2020-05-03 01:12:10 -0700

    integrating compound oracle

--- src/graphicriver-21806066-logistic-electric-vehicles-isometric-composition (1)/__tests__/config.test.ts
+++
@@ -1,9 +0,0 @@
-import {instantiateConfig} from '../config';
-
-
-test('tests config', async () => {
-  let saddle = require('../../saddle');
-  let x = await instantiateConfig(saddle, 'test');
-
-  expect(x).toEqual(5);
-});
